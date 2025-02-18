15294ba747335bcc1bb357a44ef5c4acbffb677d
Jonhnata <111892785+Jonhnata@users.noreply.github.com>
2025-02-18 23:45:35 +0000

    Programa 01 - exemplo de funções

---
+++ Estudos/Ex1.java
@@ -0,0 +1,15 @@
+public class Ex1{
+
+
+public static void main(String[] args){
+    System.out.println("Olá mundo");
+    System.out.println("Tudo bem? ");
+    
+
+    int maiorIdade = 18;
+    String torcdTime = "Sport";
+    String  nome = "Jonhnata";
+
+    System.out.println("Idade "+maiorIdade+"time "+torcdTime+"nome "+nome);
+}
+}
---
+++ ex01-hello-world/Exemplo01.java
@@ -0,0 +1,12 @@
+public class Exemplo01 {
+    public static void main(String[] args) {
+        System.out.println("Olá, \nMundo !");
+        System.out.println("o dobro de 10 é " + dobro(10));
+    }
+
+    public static int dobro(int x) {
+        return 2 * x;
+
+    }
+
+}
